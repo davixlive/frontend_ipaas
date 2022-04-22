5026e853f29c8dae49f821153f569be135259d54
davixlive <cardillo.davide.studente@itispaleocapa.it>
2022-04-22 09:52:34 +0200

    Add Sidebar

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,170 @@
 import 'package:flutter/material.dart';
+import 'package:easy_sidemenu/easy_sidemenu.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Ipaas',
+      /* theme: ThemeData(
+        primarySwatch: Colors.black,
+      ),*/
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  PageController page = PageController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SideMenu(
+            controller: page,
+            style: SideMenuStyle(
+              displayMode: SideMenuDisplayMode.auto,
+              hoverColor: Colors.blue[100],
+              selectedColor: Colors.lightBlue,
+              selectedTitleTextStyle: TextStyle(color: Colors.white),
+              selectedIconColor: Colors.white,
+              // decoration: BoxDecoration(
+              //   borderRadius: BorderRadius.all(Radius.circular(10)),
+              // ),
+              // backgroundColor: Colors.blueGrey[700]
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            title: Column(
+              children: [
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: 150,
+                    maxWidth: 150,
+                  ),
+                ),
+                Divider(
+                  indent: 8.0,
+                  endIndent: 8.0,
+                ),
+              ],
             ),
-          ],
-        ),
+            items: [
+              SideMenuItem(
+                priority: 0,
+                title: 'Dashboard',
+                onTap: () {
+                  page.jumpToPage(0);
+                },
+                icon: Icon(Icons.home),
+              ),
+              SideMenuItem(
+                priority: 1,
+                title: 'Applications',
+                onTap: () {
+                  page.jumpToPage(1);
+                },
+                icon: Icon(Icons.dashboard_customize_rounded),
+              ),
+              SideMenuItem(
+                priority: 2,
+                title: 'Git Sources',
+                onTap: () {
+                  page.jumpToPage(2);
+                },
+                icon: Icon(Icons.file_copy_rounded),
+              ),
+              SideMenuItem(
+                priority: 3,
+                title: 'Destinations',
+                onTap: () {
+                  page.jumpToPage(3);
+                },
+                icon: Icon(Icons.loop_rounded),
+              ),
+              SideMenuItem(
+                priority: 4,
+                title: 'Databases',
+                onTap: () {
+                  page.jumpToPage(4);
+                },
+                icon: Icon(Icons.storage),
+              ),
+              SideMenuItem(
+                priority: 6,
+                title: 'Exit',
+                onTap: () async {},
+                icon: Icon(Icons.exit_to_app),
+              ),
+            ],
+          ),
+          Expanded(
+            child: PageView(
+              controller: page,
+              children: [
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Dashboard',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Applications',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Git Sources',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Destinations',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  child: Center(
+                    child: Text(
+                      'Databases',
+                      style: TextStyle(fontSize: 35),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
